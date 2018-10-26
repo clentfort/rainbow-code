a71fd22fb590235fd8f857ea24b8fc77518a5b37
Christian Lentfort <christian@lentfort.com>
2018-10-26 15:54:44 +0100

    Updates

---
+++ precache-manifest.f0871edb15a7fe0db77da84341e29272.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d5e21098815e57eb0e45",
+    "url": "/rainbow-code/static/js/runtime~main.d5e21098.js"
+  },
+  {
+    "revision": "9b2cb8cb6f8c2461cd08",
+    "url": "/rainbow-code/static/js/main.9b2cb8cb.chunk.js"
+  },
+  {
+    "revision": "2fb07abeddeb892a5c52",
+    "url": "/rainbow-code/static/js/1.2fb07abe.chunk.js"
+  },
+  {
+    "revision": "9b2cb8cb6f8c2461cd08",
+    "url": "/rainbow-code/static/css/main.7fe412fd.chunk.css"
+  },
+  {
+    "revision": "ccbddc0ae847da8c7a98d14328c3fdc3",
+    "url": "/rainbow-code/index.html"
+  }
+];
