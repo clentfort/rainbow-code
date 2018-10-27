e087333d8a4f5a3fb493c846d05d05e923e19b9f
Christian Lentfort <christian@lentfort.com>
2018-10-27 15:15:30 +0100

    Updates

--- precache-manifest.bab26aafc564f11a481cca4b5631bb7e.js
+++ precache-manifest.bab26aafc564f11a481cca4b5631bb7e.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/rainbow-code/static/js/runtime~main.d5e21098.js"
   },
   {
-    "revision": "16e63c7e23d5b2c8f33c",
-    "url": "/rainbow-code/static/js/main.16e63c7e.chunk.js"
+    "revision": "38fb9675c9aed039d6c7",
+    "url": "/rainbow-code/static/js/main.38fb9675.chunk.js"
   },
   {
     "revision": "2e17bfd26d8d170d0964",
     "url": "/rainbow-code/static/js/1.2e17bfd2.chunk.js"
   },
   {
-    "revision": "16e63c7e23d5b2c8f33c",
+    "revision": "38fb9675c9aed039d6c7",
     "url": "/rainbow-code/static/css/main.9b13cede.chunk.css"
   },
   {
-    "revision": "b90ca74bbaf9360e7a3e8e8028d1cc0e",
+    "revision": "ec245982f7a1687a007eadd41ae688e1",
     "url": "/rainbow-code/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
 
 importScripts(
-  "/rainbow-code/precache-manifest.e4c823c7e0e3be3cb56e23f98db61122.js"
+  "/rainbow-code/precache-manifest.bab26aafc564f11a481cca4b5631bb7e.js"
 );
 
 workbox.clientsClaim();
